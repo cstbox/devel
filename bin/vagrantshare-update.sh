4976bae8d8ebfe87f4c145407cba1e582d5ee08a
Eric Pascual <eric.pascual@cstb.fr>
2015-12-01 11:25:03 +0100

    renamed and enhanced with checkings

---
+++ bin/vagrantshare-update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Updates the archives and Debian packages in the Vagrant shared dir for
+# CSTBox packages
+
+package_list="packages.list"
+here=$(pwd)
+vagrant_pkg_repo=$here/vagrant/cstbox-packages/
+
+function update_package {
+    pkg=$1
+
+    cd ../$pkg
+    echo "package $pkg:"
+
+    arch_file=$(fab arch | grep "tar czf" | cut -d' ' -f 3)
+    echo -e "\033[32m--> $arch_file \033[0m"
+    cp -a $arch_file $vagrant_pkg_repo
+
+    deb_file=$(fab build | grep "fakeroot dpkg" | cut -d' ' -f 5)
+    echo -e "\033[32m--> $deb_file \033[0m"
+    cp -a $deb_file $vagrant_pkg_repo
+
+    cd - > /dev/null
+}
+
+if [ ! -f $package_list ] ; then
+    echo -e "\033[31m*** $package_list file not found\033[34m"
+    exit 1
+fi
+
+if [ $# -eq 0 ] ; then
+    while read pkg ; do
+        [ -z "$pkg" ] && continue
+        update_package $pkg
+    done < $package_list
+else
+    for pkg in "$@"; do
+        if grep $pkg $package_list -q ; then
+            update_package $pkg
+        else
+            echo -e "\033[31m*** $pkg not in $package_list:\033[34m"
+            cat $package_list
+            echo -e "\033[0m"
+        fi
+    done
+fi
