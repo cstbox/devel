e91495b77fbc9af627f55ac9812e3b53981ef3e2
Eric Pascual <eric.pascual@cstb.fr>
2016-02-09 18:26:35 +0100

    added dynamic version computation based on Git tags

--- fabfile.py
+++ fabfile.py
@@ -21,7 +21,8 @@ REMOTE_TARGET_PACKAGES_DIR = "cstbox-packages/"
 
 REPOS_PATH = {
     'dropbox': os.path.expanduser("~/Dropbox-private/Dropbox/cstbox/"),
-    'ppa': os.path.expanduser("~/cstbox-workspace/ppa/"),
+    #'ppa': os.path.expanduser("~/cstbox-workspace/ppa/"),
+    'ppa': os.path.expanduser("~/Dropbox-private/Dropbox/cstbox-apt/"),
     'vagrant': os.path.join("vagrant", REMOTE_TARGET_PACKAGES_DIR),
     'vagrant.tydom': os.path.join(_HERE, "app-tydom", "vagrant", REMOTE_TARGET_PACKAGES_DIR),
     'vagrant.deltadore': os.path.join(_HERE, "ext-deltadore", "vagrant", REMOTE_TARGET_PACKAGES_DIR),
@@ -46,18 +47,27 @@ def _get_package_name_and_version():
     name = version = None
     project_root = _find_project_root()
 
-    for line in file(os.path.join(project_root, 'DEBIAN/control')):
+    deb_control = os.path.join(project_root, 'DEBIAN/control.template')
+    if not os.path.exists(deb_control):
+        deb_control = os.path.join(project_root, 'DEBIAN/control')
+        if not os.path.exists(deb_control):
+            abort('cannot find DEBIAN control or control.template file')
+
+    # look for package name in the control file
+    name = None
+    for line in file(deb_control):
         key, value = line.split(':')
         key = key.strip()
         if key == 'Package':
             name = value.strip()
-            continue
-        if key == 'Version':
-            version = value.strip()
-            continue
+            break
+    if not name:
+        abort('cannot find package name in DEBIAN control or control.template file')
 
-        if name and version:
-            return name, version
+    # get the current git version
+    version = git_version()
+
+    return name, version
 
 
 def _get_package_file_name():
@@ -75,7 +85,11 @@ def _get_debian_control_path():
 
 
 def _version_fields(version):
-    return tuple((int(s) for s in version.split('.')[:3]))
+    fields = version.split('.')
+    # add the build field if none
+    if len(fields) < 4 and not fields[-1].isdigit():
+        fields.insert(2, 0)
+    return tuple((int(s) for s in fields[:3]))
 
 
 def inc_version(version, inc_major=False, inc_minor=False, inc_build=True):
@@ -97,24 +111,28 @@ def inc_version(version, inc_major=False, inc_minor=False, inc_build=True):
 
 @task
 def inc_version_build():
+    """ Increase the build number in package version number """
     name, version = _get_package_name_and_version()
     local('git tag -a -m "%(version_str)s" %(version_str)s' % {'version_str': inc_version(version)})
 
 
 @task
 def inc_version_minor():
+    """ Increase the minor number in package version number """
     name, version = _get_package_name_and_version()
     local('git tag -a -m "%(version_str)s" %(version_str)s' % {'version_str': inc_version(version, inc_minor=True)})
 
 
 @task
 def inc_version_major():
+    """ Increase the major number in package version number """
     name, version = _get_package_name_and_version()
     local('git tag -a -m "%(version_str)s" %(version_str)s' % {'version_str': inc_version(version, inc_major=True)})
 
 
 @task
 def update_deb_version():
+    """ Updates the version field in the DEBIAN/control file (deprecated) """
     project_root = _find_project_root()
 
     new_version = git_version()
@@ -153,19 +171,22 @@ def make_deb_control():
     """ Generates de DEBIAN/control file """
     project_root = _find_project_root()
 
-    new_version = git_version()
-
-    control_path = os.path.join(project_root, 'DEBIAN', 'control')
-    template_path = control_path + '.template'
-    lines = file(template_path).readlines()
-    file(control_path, 'wt').writelines(''.join(lines) % {'version': new_version})
-    print(green('%s version field updated to "%s"' % (control_path, new_version)))
+    try:
+        new_version = git_version()
+    except ValueError:
+        print(error("cannot determine the version number. Did you create a git tag for it ?"))
+        abort()
+    else:
+        control_path = os.path.join(project_root, 'DEBIAN', 'control')
+        template_path = control_path + '.template'
+        lines = file(template_path).readlines()
+        file(control_path, 'wt').writelines(''.join(lines) % {'version': new_version})
+        print(green('%s version field updated to "%s"' % (control_path, new_version)))
 
 
 @task
 def build_deb():
-    """ Generates the Debian package
-    """
+    """ Generates the Debian package """
     with lcd(_find_project_root()):
         if os.path.exists(os.path.join('DEBIAN', 'control.template')):
             execute(make_deb_control)
@@ -178,8 +199,7 @@ def build_deb():
 
 @task
 def build_arch():
-    """ Generates a deployable archive of the package
-    """
+    """ Generates a deployable archive of the package """
     new_version = git_version()
     with lcd(_find_project_root()):
         execute(update__version__)
@@ -188,15 +208,13 @@ def build_arch():
 
 @task
 def deploy():
-    """ Deploys the Debian package to the target
-    """
+    """ Deploys the Debian package to the target """
     put(_get_package_file_name(), REMOTE_TARGET_PACKAGES_DIR)
 
 
 @task
 def install(name=''):
-    """ Remotely installs the Debian package
-    """
+    """ Remotely installs the Debian package (parms: name=<current one>)"""
     pkg_name = name or _get_package_file_name()
     pkg_dir_prefix = '/vagrant' if env.user == 'vagrant' else ''
     sudo('service cstbox stop')
@@ -206,8 +224,7 @@ def install(name=''):
 
 @task
 def publish(to='vagrant'):
-    """ Copies the Debian package to the repository
-    """
+    """ Copies the Debian package to a repository (parms: to=vagrant) """
     try:
         project_root = _find_project_root()
         pkg_fn = os.path.join(project_root, _get_package_file_name())
@@ -225,6 +242,7 @@ def publish(to='vagrant'):
 
 @task
 def sign_deb():
+    """ Signs the Debian package"""
     with lcd(_find_project_root()):
         local('dpkg-sig -k cstbox --sign builder %s' % _get_package_file_name())
 
@@ -234,6 +252,7 @@ PPA_KEEP_MULTIPLE_VERSION = False
 
 @task
 def update_ppa():
+    """ Updates the PPA with the most recent package version"""
     execute(publish, to='ppa')
     execute(sign_deb)
     with lcd(REPOS_PATH['ppa']):
@@ -247,6 +266,7 @@ def update_ppa():
 @with_settings(user='root', password='root')
 @task
 def tydom_deploy():
+    """ Deploys a package archive on a connected Tydom"""
     # local('scp -r lib/python/pycstbox/* root@tydom:/opt/cstbox/lib/python/pycstbox/')
     project_root = _find_project_root()
     arch_name = _get_package_arch_name()
@@ -257,16 +277,20 @@ def tydom_deploy():
 @with_settings(user='root', password='root')
 @task
 def tydom_install():
+    """ Installs a package on a connected Tydom"""
     user_home = '/root' if env.user == 'root' else os.path.join('/home', env.user)
     arch_name = _get_package_arch_name()
-    with cd("/"):
-        run('tar xf %s' % os.path.join(user_home, REMOTE_TARGET_PACKAGES_DIR, arch_name))
+    # with cd("/"):
+    #     run('tar xf %s' % os.path.join(user_home, REMOTE_TARGET_PACKAGES_DIR, arch_name))
+    with cd(os.path.join(user_home, REMOTE_TARGET_PACKAGES_DIR)):
+        run('./install.sh ' + arch_name)
 
 
 @hosts('tydom')
 @with_settings(user='root', password='root')
 @task
 def tydom_cbx_restart():
+    """ Restarts CSTBox services on a connected Tydom"""
     run("/etc/init.d/cstbox stop")
     time.sleep(5)
     run("/etc/init.d/cstbox start")
@@ -274,6 +298,7 @@ def tydom_cbx_restart():
 
 @task
 def tydom_all():
+    """ Builds, deploys and installs a package on a connected Tydom """
     execute(build_arch)
     execute(tydom_deploy)
     execute(tydom_install)
@@ -285,6 +310,7 @@ DOCUMENTATION_SUBDIRS = ['./docs', './doc']
 
 @task
 def doc(output_format='html', clean=False):
+    """ Generates the package documentation. (parms: output_format=html, clean=False) """
     doc_subdir = None
     for d in DOCUMENTATION_SUBDIRS:
         if os.path.isdir(d):
