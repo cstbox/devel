778401750cda0d74306838b8c75947f3d1ad87a7
Eric Pascual <eric.pascual@cstb.fr>
2016-08-26 10:53:17 +0200

    filter empty lines in packages list

--- src/cstbox_devtools/fabtasks.py
+++ src/cstbox_devtools/fabtasks.py
@@ -5,6 +5,8 @@ import time
 import os
 import re
 import shutil
+import glob
+import subprocess
 
 from fabric.api import local, put, sudo, cd, env, hosts, run, execute, lcd, task
 from fabric.context_managers import settings
@@ -19,50 +21,14 @@ __author__ = 'Eric Pascual - CSTB (eric.pascual@cstb.fr)'
 
 _HERE = os.path.dirname(__file__)
 
-# EXTRA_SETTINGS = 'fabfile-settings.py'
 REMOTE_TARGET_PACKAGES_DIR = "cstbox-packages/"
-
-# local_settings_path = os.path.join(os.getcwd(), EXTRA_SETTINGS)
-# shared_settings_path = os.path.join(_HERE, EXTRA_SETTINGS)
-#
-# extra_hosts = []
-# extra_repos = {}
 
 LOCAL_REPOS_ROOT = os.path.expanduser("~/cstbox-workspace/ppa/")
 REPOS_PATH = {
-    # 'dropbox': os.path.expanduser("~/Dropbox-private/Dropbox/cstbox/"),
-    # 'ppa': os.path.expanduser("~/cstbox-workspace/ppa/"),
-    # 'vagrant': os.path.join("vagrant", REMOTE_TARGET_PACKAGES_DIR),
-    # 'vagrant.tydom': os.path.join('%(here)s', "app-tydom", "vagrant", "%(remote_target_packages_dir)s"),
-    # 'vagrant.deltadore': os.path.join('%(here)s', "ext-deltadore", "vagrant", "%(remote_target_packages_dir)s"),
-    # 'vagrant.wsfeed': os.path.join('%(here)s', "ext-wsfeed", "vagrant", "%(remote_target_packages_dir)s"),
 }
 
-
-# for p in (shared_settings_path, local_settings_path):
-#     if os.path.exists(p):
-#         context = {
-#             "here": _HERE,
-#             "remote_target_packages_dir": REMOTE_TARGET_PACKAGES_DIR
-#         }
-#         settings_module = imp.load_source('fab_settings', p)
-#         if hasattr(settings_module, 'hosts'):
-#             extra_hosts.extend([tmpl % context for tmpl in settings_module.hosts])
-#         if hasattr(settings_module, 'REPOS_PATH'):
-#             extra_repos.update({k: v % context for k, v in settings_module.REPOS_PATH.iteritems()})
-#
-# if extra_repos:
-#     REPOS_PATH.update(extra_repos)
-
-# ssh.util.log_to_file("paramiko.log", 10)
 env.use_ssh_config = True
 env.no_agent = True
-
-# if extra_hosts:
-#     if env.hosts is not None:
-#         env.hosts.extend(extra_hosts)
-#     else:
-#         env.hosts = extra_hosts
 
 
 def git_version():
@@ -262,26 +228,34 @@ def publish(to='vagrant', addit_copy_func=None):
     """ Copies the Debian package to a repository (parms: to=vagrant) """
     try:
         project_root = _find_project_root()
-        pkg_fn = os.path.join(project_root, _get_package_file_name())
+        pkg_fn = _get_package_file_name()
+
     except IOError:
         error('no Debian package defined for this component')
+
     else:
-        if os.path.isfile(pkg_fn):
-            try:
-                local('cp -a %s %s' % (pkg_fn, REPOS_PATH[to]))
-                if addit_copy_func:
-                    addit_copy_func(pkg_fn, REPOS_PATH[to])
-            except KeyError:
-                error('repository "%s" does not exist' % to)
-        else:
-            error('Debian package %s not yet generated' % pkg_fn)
+        pkg_fpath = os.path.join(project_root, 'dist', pkg_fn)
+        if not os.path.isfile(pkg_fpath):
+            pkg_fpath = os.path.join(project_root, pkg_fn)
+            if not os.path.isfile(pkg_fpath):
+                error('Debian package %s not yet generated' % pkg_fn)
+
+        try:
+            local('cp -a %s %s' % (pkg_fpath, REPOS_PATH[to]))
+            if addit_copy_func:
+                addit_copy_func(pkg_fpath, REPOS_PATH[to])
+        except KeyError:
+            error('repository "%s" does not exist' % to)
 
 
 @task
 def sign_deb():
     """ Signs the Debian package"""
     with lcd(_find_project_root()):
-        local('dpkg-sig -k cstbox --sign builder %s' % _get_package_file_name())
+        pkg_fn = _get_package_file_name()
+        if not os.path.exists(pkg_fn):
+            pkg_fn = os.path.join('dist', pkg_fn)
+        local('dpkg-sig -k cstbox --sign builder %s' % pkg_fn)
 
 
 PPA_NAME = None
@@ -289,6 +263,17 @@ VISIBLE_ON_PUBLIC_SERVER = False
 PPA_KEEP_MULTIPLE_VERSION = True
 
 PPA_DIR_PATH = "/var/www/cstbox-ppa/"
+
+
+@hosts('tydom')
+@with_settings(user='root', password='root')
+@task
+def tydom_deploy():
+    """ Deploys a package archive on a connected Tydom"""
+    # local('scp -r lib/python/pycstbox/* root@tydom:/opt/cstbox/lib/python/pycstbox/')
+    project_root = _find_project_root()
+    arch_name = _get_package_arch_name()
+    local('scp -r %s root@tydom:%s' % (os.path.join(project_root, arch_name), REMOTE_TARGET_PACKAGES_DIR))
 
 
 def do_update_ppa(ppa_name=None, local_only=True, addit_copy_func=None):
@@ -314,17 +299,6 @@ def do_update_ppa(ppa_name=None, local_only=True, addit_copy_func=None):
             if VISIBLE_ON_PUBLIC_SERVER or ppa_name == 'public':
                 with settings(warn_only=True):
                     local(cmde % (ppa_name, 'public'))
-
-
-@hosts('tydom')
-@with_settings(user='root', password='root')
-@task
-def tydom_deploy():
-    """ Deploys a package archive on a connected Tydom"""
-    # local('scp -r lib/python/pycstbox/* root@tydom:/opt/cstbox/lib/python/pycstbox/')
-    project_root = _find_project_root()
-    arch_name = _get_package_arch_name()
-    local('scp -r %s root@tydom:%s' % (os.path.join(project_root, arch_name), REMOTE_TARGET_PACKAGES_DIR))
 
 
 @hosts('tydom')
@@ -401,3 +375,41 @@ def deb_clean():
     else:
         print("... no obsolete package found.")
 
+
+@task(alias="updpkg")
+def vagrant_updpkg():
+    project_root = _find_project_root()
+    vagrant_dir = os.path.join(project_root, 'vagrant')
+    if not os.path.isdir(vagrant_dir):
+        abort('no Vagrant environment defined')
+
+    pkg_list_file = os.path.join(vagrant_dir, 'packages.list')
+    if not os.path.isfile(pkg_list_file):
+        abort('packages.list file not found')
+
+    vagrant_pkgs_dir = os.path.join(vagrant_dir, 'cstbox-packages')
+    print('clearing packages in %s...' % vagrant_pkgs_dir)
+    for f in glob.glob(vagrant_pkgs_dir + '/*.deb'):
+        os.path.join(vagrant_pkgs_dir, f)
+
+    pkg_install_script = os.path.join(vagrant_dir, 'install-packages.sh')
+    with open(pkg_list_file) as fp, open(pkg_install_script, 'w') as fp_out:
+        for rec in fp:
+            rec = rec.strip()
+            if not rec:
+                continue
+
+            pkg_name, version = (rec.strip() + '=*').split('=')[:2]
+            print('retrieving package %s (version: %s)...' % (pkg_name, 'latest' if version == '*' else version))
+            _, short_name = pkg_name.split('-', 1)
+            src_dist_dir = os.path.join(os.path.dirname(project_root), short_name, 'dist')
+            if version != '*':
+                src_deb_path = os.path.join(src_dist_dir, "%s_%s_all.deb" % (pkg_name, version))
+            else:
+                src_deb_path = os.path.join(src_dist_dir, "%s.deb" % pkg_name)
+
+            if os.path.exists(src_deb_path):
+                subprocess.call('cp -aL %s %s' % (src_deb_path, vagrant_pkgs_dir), shell=True)
+                fp_out.write("dpkg -i /vagrant/cstbox-packages/%s\n" % os.path.basename(src_deb_path))
+            else:
+                print('ERROR: file not found (%s)' % src_deb_path)
